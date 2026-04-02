e6dd905c7ffe42720dc8e72953ca3b92fab73113
Sascha Märkl <dev@maerkl.net>
2026-04-02 13:20:02 +0200

    feat: add isManualPrice extension property to Article

--- app/src/main/java/net/maerkl/kassierapp/data/local/Article.kt
+++ app/src/main/java/net/maerkl/kassierapp/data/local/Article.kt
@@ -3,6 +3,8 @@ package net.maerkl.kassierapp.data.local
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+const val MANUAL_PRICE_ARTICLE_NAME = "__MANUAL_PRICE__"
+
 @Entity(tableName = "articles")
 data class Article(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
@@ -13,3 +15,6 @@ data class Article(
     val isActive: Boolean = true,
     val collectionId: Long = 1
 )
+
+val Article.isManualPrice: Boolean
+    get() = name == MANUAL_PRICE_ARTICLE_NAME
